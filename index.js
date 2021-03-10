cd52d90a95cb3c99a063538f9aeb8b6d2c5cb672
Andrzej Polak <andrzejp2805@gmail.com>
2021-03-10 22:50:53 +0100

    Add function to get a scroll data

--- index.js
+++ index.js
@@ -3,9 +3,20 @@ document.addEventListener('DOMContentLoaded', function () {
 
   const rootElement = document.querySelector('#root');
   const sections = document.querySelectorAll('section');
+  let currentSectionIndex = 0;
+
 
 
   document.addEventListener('mousewheel', function (e) {
-    console.log(e.wheelDelta);
+    const direction = e.wheelDelta < 0 ? 1 : -1;
+    if (direction === 1) {
+      const isLastSection = currentSectionIndex === sections.length - 1;
+      if (isLastSection) return;
+    } else if (direction === -1) {
+      const firstSection = currentSectionIndex === 0;
+      if (firstSection) return;
+    }
+    currentSectionIndex = currentSectionIndex + direction;
+    console.log(currentSectionIndex);
   })
 })
